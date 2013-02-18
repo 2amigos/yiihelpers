8ed5c4adb7672f1a19f22a979320f6421a7cb339
mtabin <matt@clevertech.biz>
2013-02-17 23:12:51 -0500

    tab spacing fix

--- helpers/FinanceLibHelper.php
+++ helpers/FinanceLibHelper.php
@@ -16,17 +16,17 @@ class FinanceLibHelper
 	 *  used for black-scholes pricing formula
 	 */
 	public static function cnd($val) {
-    $p = floatval(0.2316419);
-    $b1 = floatval(0.319381530);
-    $b2 = floatval(-0.356563782);
-    $b3 = floatval(1.781477937);
-    $b4 = floatval(-1.821255978);
-    $b5 = floatval(1.330274429);
-    $t = 1/(1 + ($p * floatval($val)));
-    $zx = (1/(sqrt(2 * pi())) * (exp(0 - pow($val, 2)/2)));
+		$p = floatval(0.2316419);
+		$b1 = floatval(0.319381530);
+		$b2 = floatval(-0.356563782);
+		$b3 = floatval(1.781477937);
+		$b4 = floatval(-1.821255978);
+		$b5 = floatval(1.330274429);
+		$t = 1/(1 + ($p * floatval($val)));
+		$zx = (1/(sqrt(2 * pi())) * (exp(0 - pow($val, 2)/2)));
 
-    $px = 1 - floatval($zx) * (($b1 * $t) + ($b2 * pow($t, 2)) + ($b3 * pow($t, 3)) + ($b4 * pow($t, 4)) + ($b5 * pow($t,5)));
-    return $px;
+		$px = 1 - floatval($zx) * (($b1 * $t) + ($b2 * pow($t, 2)) + ($b3 * pow($t, 3)) + ($b4 * pow($t, 4)) + ($b5 * pow($t,5)));
+		return $px;
 	}
 
 	/**
@@ -46,12 +46,14 @@ class FinanceLibHelper
 	 */
 	 
 	public static function BlackScholesCalculator ($callPutFlag, $currAssetPrice, $exercisePrice, $timeToMaturity, $riskFreeInterestRate, $annualVolatility) {
-    $d1 = ( log($currAssetPrice / $exercisePrice) + ($riskFreeInterestRate + (pow($annualVolatility,2)) / 2) * $timeToMaturity) / ($annualVolatility * (pow($timeToMaturity,0.5)));
-    $d2 = $d1 - $annualVolatility * (pow($timeToMaturity,0.5));
+		$d1 = ( log($currAssetPrice / $exercisePrice) + ($riskFreeInterestRate + (pow($annualVolatility,2)) / 2) * $timeToMaturity) / ($annualVolatility * (pow($timeToMaturity,0.5)));
+		$d2 = $d1 - $annualVolatility * (pow($timeToMaturity,0.5));
 
-    if ($callPutFlag === 'c')
-        return $currAssetPrice * self::cnd($d1) - $exercisePrice * exp(-$riskFreeInterestRate * $timeToMaturity) * self::cnd($d2);
-    else
-        return $exercisePrice * exp(-$riskFreeInterestRate * $timeToMaturity) * self::cnd(-$d2) - $currAssetPrice * self::cnd(-$d1);
+		if ($callPutFlag === 'c') 
+		{
+			return $currAssetPrice * self::cnd($d1) - $exercisePrice * exp(-$riskFreeInterestRate * $timeToMaturity) * self::cnd($d2);
+		} else {
+			return $exercisePrice * exp(-$riskFreeInterestRate * $timeToMaturity) * self::cnd(-$d2) - $currAssetPrice * self::cnd(-$d1);
+		}
 	}
 }
